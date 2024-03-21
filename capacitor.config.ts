c2b12fbb3a856e3009bc71084ee4941aa79313b3
Guilherme Henrique <guilhermehtk@hotmail.com>
2024-03-21 09:48:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Currency Converter',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
